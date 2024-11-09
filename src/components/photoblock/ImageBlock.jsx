ea83df113366fff39c5864cb3326232a2c46e0bf
Crescent Dede <crescentdede@gmail.com>
2024-11-09 11:14:41 +0100

    show more, less buttons

--- src/components/photoblock/ImageBlock.jsx
+++ src/components/photoblock/ImageBlock.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { LuView } from "react-icons/lu";
 import { FaShoppingCart } from "react-icons/fa";
 import { CartList } from "../../context/CartItemContext";
@@ -13,6 +13,20 @@ function ImageBlock({ el, hidden: hide, status }) {
   const { setIsShow } = useContext(MyContext);
   const { setData, setShowPreview, smallPrev, setSmallPrev } =
     useContext(PreviewData);
+  const [isShowMore, setIsShowMore] = useState(false);
+  const [description, setDescription] = useState("");
+  useEffect(() => {
+    let truncateDescription = () => {
+      if (isShowMore) {
+        return el.title + " ";
+      }
+      return el.title.slice(0, 20) + "... ";
+    };
+    setDescription(truncateDescription);
+  }, [isShowMore]);
+  const toggleFullDescription = () => {
+    setIsShowMore(!isShowMore);
+  };
 
   return (
     <div
@@ -27,7 +41,7 @@ function ImageBlock({ el, hidden: hide, status }) {
     >
       {isOnMouseOver && (
         <div className="flex absolute justify-between left-0 w-full">
-          <button //homepage preview button 
+          <button //homepage preview button
             className={`rounded-md max-md:hidden bg-opacity-50 z-50 p-4 border border-transparent text-sm font-medium text-white bg-black hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800   
  focus:ring-indigo-500 ${hide}`}
             onClick={() => {
@@ -50,14 +64,15 @@ function ImageBlock({ el, hidden: hide, status }) {
           >
             <LuView className="scale-150" />
           </button>
-          <button disabled={cartItems.includes(el.id)}
+          <button
+            disabled={cartItems.includes(el.id)}
             className="rounded-md bg-opacity-50 z-50 p-4 border borders-transparent text-sm font-medium text-white bg-black hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800   
  focus:ring-indigo-500"
             onClick={() => {
               // addToCart(el),
               // getTotalPriceForItem(el),
               setTotalPrice(totalPrice + el.price);
-              setCartItems([...cartItems, {...el, qty:el.qty || 1}]);
+              setCartItems([...cartItems, { ...el, qty: el.qty || 1 }]);
               setIsShow(true);
             }}
           >
@@ -71,8 +86,12 @@ function ImageBlock({ el, hidden: hide, status }) {
         alt={el.description}
       />
       <div className="max-[568px]: bg-[#242424] px-5 pt-2 pb-1 rounded">
-        <p className="font-serif text-white font-extrabold">
-          {el.title.length > 20 ? el.title.slice(0, 20)+"..." : el.title}
+        <p className="text-white font-sans">
+          {description}
+
+          <button className="text-green-600" onClick={toggleFullDescription}>
+            {isShowMore ? "less" : "more"}
+          </button>
         </p>
         <p className="text-white py-2">${el.price}</p>
       </div>
